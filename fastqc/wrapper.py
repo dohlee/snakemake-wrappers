f797de8b20a665d9142262a4ad6ec3f0c89f6aef
dohlee <apap950419@gmail.com>
2018-06-29 13:08:52 +0900

    Write FastQC wrappers.

---
+++ fastqc/wrapper.py
@@ -0,0 +1,33 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Extract parameters.
+extra = snakemake.params.get('extra', '')
+
+# Extract required arguments.
+assert isinstance(list, snakemake.input), 'Sequencing read files should be supplied as a list.'
+read_command = ' '.join(snakemake.input)
+
+html = snakemake.output.html
+zipped = snakemake.output.zip
+output_directory = path.dirname(html)
+
+# Execute shell command.
+shell(
+    "fastqc "
+    "-o {output_directory} "
+    "-t {snakemake.threads} "
+    "{extra} "
+    "{read_command} "
+    "{log}"
+)
