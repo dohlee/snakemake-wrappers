e58ad5933f943e90d592ab81c5d3e3f8a2f0c0bc
dohlee <apap950419@gmail.com>
2018-06-29 16:35:23 +0900

    Write samtools wrappers.

---
+++ samtools/index/wrapper.py
@@ -0,0 +1,30 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Extract parameters.
+extra = snakemake.params.get('extra', '')
+
+# Extract required arguments.
+input = snakemake.input[0]
+output = snakemake.output[0]
+
+# Execute shell command.
+shell(
+    "("
+    "samtools index "
+    "{extra} "
+    "{input} "
+    "{output} "
+    ")"
+    "{log}"
+)
---
+++ samtools/sort/wrapper.py
@@ -0,0 +1,33 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Extract parameters.
+extra = snakemake.params.get('extra', '')
+
+# Extract required arguments.
+input = snakemake.input[0]
+output = snakemake.output[0]
+prefix = path.splitext(output)
+
+# Execute shell command.
+shell(
+    "("
+    "samtools sort "
+    "{extra} "
+    "-@ {snakemake.threads} "
+    "-o {output} "
+    "-T {prefix} "
+    "{input}"
+    ")"
+    "{log}"
+)
