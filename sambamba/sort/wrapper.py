cf8aa148e92e74096e47b36790fed33df7424ea0
dohlee <apap950419@gmail.com>
2018-08-01 00:59:06 +0900

    Write sambamba/index and sambamba/sort wrappers.

---
+++ sambamba/sort/wrapper.py
@@ -0,0 +1,28 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Extract parameters.
+extra = snakemake.params.get('extra', '')
+
+# Extract required arguments.
+input = snakemake.input[0]
+output = snakemake.output[0]
+
+# Execute shell command.
+shell(
+    "sambamba sort "
+    "{extra} "
+    "-t {snakemake.threads} "
+    "-o {output} "
+    "-p "
+    "{input}"
+    "{log}"
+)
