c47a84cbf1afa12d588738cd4330a56531c436ce
dohlee <apap950419@gmail.com>
2018-08-03 13:13:16 +0900

    Write bismark/genome-preparation wrapper.

---
+++ bismark/genome-preparation/wrapper.py
@@ -0,0 +1,28 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Extract parameters.
+extra = snakemake.params.get('extra', '')
+
+# Extract required arguments.
+reference_dir = snakemake.input
+
+# Execute shell command.
+shell(
+    "("
+    "bismark_genome_preparation "
+    "{extra} "
+    "{reference_dir} "
+    ")"
+    "{log}"
+)
