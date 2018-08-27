ce4b4ac49382bb3545fc0ce0cd2576a65e3f64c3
dohlee <apap950419@gmail.com>
2018-08-27 17:20:26 +0900

    Write bwa/mem wrapper and rule.

---
+++ bwa/mem/rule.smk
@@ -0,0 +1,23 @@
+rule bwa_mem:
+    input:
+        # Required input. Reference genome fasta file.
+        reads = ['{sample}.read1.fastq.gz'],
+        mates = ['{sample}.read2.fastq.gz'],
+        # You may use any of {genome}.amb, {genome}.ann, {genome}.bwt,
+        # {genome}.pac, {genome}.sa just to obligate snakemake to run `bwa index` first.
+        reference = 'reference/hg38.bwt'
+    output:
+        # BAM output or SAM output is both allowed.
+        # Note that BAM output will be automatically detected by its file extension,
+        # and SAM output (which is bwa mem default) will be piped through `samtools view`
+        # to convert SAM to BAM.
+        'result/{sample}/{sample}.bam'
+    params:
+        # -M option marks secondary alignments.
+        # You may need this if you use GATK downstream.
+        extra = "-M" \
+                # Read group annotation. Omit if unused.
+                "-R '@RG\tID:{sample}\tSM:{sample}'",
+    threads: 8
+    wrapper:
+        'http://dohlee-bio.info:9193/bwa/mem'
---
+++ bwa/mem/wrapper.py
@@ -0,0 +1,52 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Extract parameters.
+extra = snakemake.params.get('extra', '')
+
+# Extract required inputs.
+reads = snakemake.input.reads
+mates = snakemake.input.get('mates', '')
+reference = snakemake.input.referece
+
+db_prefix = path.join(path.dirname(reference), path.splitext(reference)[0])
+
+# If -p option is present, 'mates' will be ignored and bwa-mem will assume 2i and 2i+1-th read files are paired.
+input_is_interleaved = '-p' in extra
+if input_is_interleaved and mates != '':  # Kindly reformat input data for mistaken input.
+    reads = [file for pair in zip(read, mate) for file in pair]  # Interleave two lists!
+    mates = ''
+
+# Extract required outputs.
+output = snakemake.output[0]
+
+pipe_command = ''
+# bwa-mem output defaults to sam. Convert sam to bam with samtools.
+# TODO: Use sambamba with appropriate number of threads.
+if output.endswith('.bam'):
+    pipe_command = '| samtools view -Sb -'
+
+# Execute shell command.
+shell(
+    "("
+    "bwa mem "
+    "{extra} "
+    "-t {snakemake.threads} "
+    "{db_prefix} "
+    "{reads} "
+    "{mates} "
+    ") "
+    "{pipe_command} > "
+    "{output} "
+    "{log}"
+)
