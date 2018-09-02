272a9cb5b6d1b3aa3ad0455299f44dbe85635863
dohlee <apap950419@gmail.com>
2018-09-02 16:46:21 +0900

    Write htseq-count wrapper.

---
+++ htseq/count/wrapper.py
@@ -0,0 +1,65 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+def optionify_input(parameter, option):
+    """Return optionified parameter."""
+    try:
+        param = str(snakemake.input[parameter])
+        if param:
+            return option + ' ' + str(snakemake.input[parameter])
+        else:
+            return ''
+    except AttributeError:
+        return ''
+
+def optionify_params(parameter, option):
+    """Return optionified parameter."""
+    try:
+        param = str(snakemake.params[parameter])
+        if param:
+            return option + ' ' + str(snakemake.params[parameter])
+        else:
+            return ''
+    except AttributeError:
+        return ''
+
+# Extract required inputs.
+alignment = snakemake.input.alignment
+annotation = snakemake.input.annotation
+
+# Extract required outputs.
+output = snakemake.output[0]
+
+# Extract parameters.
+# Extract optional parameters.
+extra = snakemake.params.get('extra', '')
+file_format_option = ''
+if alignment.endswith('.bam') and '-f bam' not in extra:
+    file_format_option = '-f bam'
+
+mode_option = optionify_params('mode', '--mode')
+stranded_option = optionify_params('stranded', '--stranded')
+
+# Execute shell command.
+shell(
+    "("
+    "htseq-count "
+    "{extra} "
+    "{file_format_option} "
+    "{mode_option} "
+    "{stranded_option} "
+    "{alignment} "
+    "{annotation}"
+    ") > {output} "
+    "{log}"
+)
