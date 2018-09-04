3fd12fc8b4ced7030b51308c5d2dd0c563abd3ab
dohlee <apap950419@gmail.com>
2018-09-04 20:51:03 +0900

    Fix bugs regarding output directory options in trim-galore wrapper.

--- trim-galore/wrapper.py
+++ trim-galore/wrapper.py
@@ -32,19 +32,23 @@ else:
     read_command = '--paired %s %s' % (a, b)
 
 output_directory = path.dirname(snakemake.output[0])
+output_directory_option = ''
+if output_directory != '':
+    output_directory_option = '-o ' + output_directory
+
 # NOTE: For paired-end case, we rename output file *.read1_val_1.fq.gz into *.read1.trimmed.fastq.gz,
 # and *.read2_val_2.fq.gz into *.read2.trimmed.fastq.gz
 if len(snakemake.input) == 2:
     # Extract sample name.
     for output in snakemake.output:
         if output.endswith('.read1.trimmed.fastq.gz'):
-            sample_name = output[:-23]
+            sample_name = path.basename(output)[:-23]
 
-    raw_read1_file = '%s.read1_val_1.fq.gz' % sample_name
-    renamed_read1_file = '%s.read1.trimmed.fastq.gz' % sample_name
+    raw_read1_file = path.join(output_directory, '%s.read1_val_1.fq.gz' % sample_name)
+    renamed_read1_file = path.join(output_directory, '%s.read1.trimmed.fastq.gz' % sample_name)
 
-    raw_read2_file = '%s.read2_val_2.fq.gz' % sample_name
-    renamed_read2_file = '%s.read2.trimmed.fastq.gz' % sample_name
+    raw_read2_file = path.join(output_directory, '%s.read2_val_2.fq.gz' % sample_name)
+    renamed_read2_file = path.join(output_directory, '%s.read2.trimmed.fastq.gz' % sample_name)
 
     rename_command = '&& mv %s %s ' \
                      '&& mv %s %s ' %\
@@ -54,9 +58,9 @@ if len(snakemake.input) == 2:
 # NOTE: For single-end case, we rename *_trimmed.fastq.gz into *.trimmed.fastq.gz.
 else:
     # Extract sample name from *.fastq.gz
-    sample_name = snakemake.input[0][:-9]
-    raw_read_file = '%s_trimmed.fq.gz' % sample_name
-    renamed_read_file = '%s.trimmed.fastq.gz' % sample_name
+    sample_name = path.basename(snakemake.input[0])[:-9]
+    raw_read_file = path.join(output_directory, '%s_trimmed.fq.gz' % sample_name)
+    renamed_read_file = path.join(output_directory, '%s.trimmed.fastq.gz' % sample_name)
     rename_command = '&& mv %s %s' % (raw_read_file, renamed_read_file)
 
 # If user wants output files to be gzipped, but did not specified --gzip option,
@@ -72,7 +76,7 @@ shell(
     "trim_galore "
     "{extra} "
     "{read_command} "
-    "-o {output_directory} "
+    "{output_directory_option} "
     "{extra} "
     "{rename_command}"
     ")"
