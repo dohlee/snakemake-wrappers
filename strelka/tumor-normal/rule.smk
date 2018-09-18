52f0ca2247e0be4b8bb2cf915cb8f5bfdbb60354
dohlee <apap950419@gmail.com>
2018-09-18 14:54:06 +0900

    Write strelka wrappers.

---
+++ strelka/tumor-normal/rule.smk
@@ -0,0 +1,18 @@
+rule strelka:
+    input:
+        # Required input.
+        tumor = '{tumor_sample}.bam',
+        normal = '{normal_sample}.bam',
+        reference = 'reference/Homo_sapiens_assembly38.fasta'
+    output:
+        # Required output.
+        snv = 'result/{tumor_sample}/{tumor_sample}_vs_{normal_sample}.snvs.strelka.vcf.gz',
+        indel = 'result/{tumor_sample}/{tumor_sample}_vs_{normal_sample}.indels.strelka.vcf.gz'
+    params:
+        # Optional parameters. Omit if unneeded.
+        extra = '',
+        region = '',  # e.g. chr2:100-2000.
+        call_regions = '',  # Here goes a predefined bed file.
+    threads: 4
+    wrapper:
+        'http://dohlee-bio.info:9193/strelka'
---
+++ strelka/tumor-normal/wrapper.py
@@ -0,0 +1,66 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2018, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+import sys
+import time
+from os import path
+
+from snakemake.shell import shell
+
+def optionify_input(parameter, option):
+    """Return optionified parameter."""
+    try:
+        return option + ' ' + snakemake.input[parameter]
+    except AttributeError:
+        return ''
+
+def optionify_params(parameter, option):
+    """Return optionified parameter."""
+    try:
+        return option + ' ' + snakemake.params[parameter]
+    except AttributeError:
+        return ''
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+# Extract parameters.
+extra = snakemake.params.get('extra', '')
+extra += optionify_params('region', '--region')
+extra += optionify_params('call_regions', '--callRegions')
+
+# Generate a name of temporary working directory.
+run_directory = 'strelka_tmp_%d' % int(time.time() * 100)
+
+# Extract required input arguments.
+reference = snakemake.input.reference
+tumor_bam = snakemake.input.tumor
+normal_bam = snakemake.input.normal
+
+# Extract output
+snv_output = snakemake.output.snv
+indel_output = snakemake.output.indel
+
+# Strelka is run in two Steps: configuration and execution.
+configure_command = 'configureStrelkaSomaticWorkflow.py --tumorBam %s --normalBam %s --ref %s --runDir %s %s' % \
+                    (tumor_bam, normal_bam, reference, run_directory, extra)
+execution_command = '%s/runWorkflow.py -m local -j %d' % (run_directory, snakemake.threads)
+
+# Move strelka results to desired output directory.
+move_command = 'mv %s/results/variants/somatic.snvs.vcf.gz %s && mv %s/results/variants/somatic.indels.vcf.gz %s' % \
+                (run_directory, snv_output, run_directory, indel_output)
+
+# Clean temporary directories.
+clean_command = 'rm -r ' + run_directory
+
+# Execute shell command.
+shell(
+    "("
+    "{configure_command} && "
+    "{execution_command} && "
+    "{move_command} && "
+    "{clean_command}"
+    ")"
+    "{log}"
+)
