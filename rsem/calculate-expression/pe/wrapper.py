e5fbe42de1588f63c3738fb9b0fae71879f89275
dohlee <dohlee.bioinfo@gmail.com>
2019-03-09 13:11:24 +0900

    Make RSEM/calculate-expression wrapper python3 compatible.

--- rsem/calculate-expression/pe/wrapper.py
+++ rsem/calculate-expression/pe/wrapper.py
@@ -14,7 +14,7 @@ def all_same(x):
 
 
 def get_prefix_of_strings(strings):
-    char_tuples = itertools.izip(*strings)
+    char_tuples = zip(*strings)
     prefix_tuples = itertools.takewhile(all_same, char_tuples)
     return ''.join(x[0] for x in prefix_tuples)
 
