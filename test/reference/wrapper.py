32698deb12e8bf84985f6fb87c89fe52ecd316be
dohlee <dohlee.bioinfo@gmail.com>
2019-06-07 15:36:25 +0900

    Update test/reference wrapper to be more flexible for the choice of references.

--- test/reference/wrapper.py
+++ test/reference/wrapper.py
@@ -17,10 +17,20 @@ else:
     wget_output = output + '.gz'
     gunzip_command = 'gunzip %s' % wget_output
 
+BASE_URL = 'https://dohlee-bioinfo.sgp1.digitaloceanspaces.com/test-data/reference/'
+if 'GRCh38' in output:
+    url = BASE_URL + 'Homo_sapiens.GRCh38.genome.chr20.fasta.gz'
+elif:
+    url = BASE_URL + 'Homo_sapiens.hg38.genome.chr20.fasta.gz'
+elif:
+    url = BASE_URL + 'Mus_musculus.mm10.genome.chr19.fasta.gz'
+else:
+    raise ValueError('Please specify "GRCh38" or "hg38" or "mm10" in your output file.')
+
 # Execute shell command.
 shell(
     "("
-    "wget https://dohlee-bioinfo.sgp1.digitaloceanspaces.com/test-data/reference/Homo_sapiens.GRCh38.genome.chr20.fasta.gz -qO {wget_output} && "
+    "wget {url} -qO {wget_output} && "
     "{gunzip_command}"
     ")"
 )
