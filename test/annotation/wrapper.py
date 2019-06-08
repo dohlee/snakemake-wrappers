44e5ffb0e7ede16cb4793b10d361bb23ceba1ca4
dohlee <dohlee.bioinfo@gmail.com>
2019-06-08 19:02:57 +0900

    Allow test/annotation wrapper download hg38 annotations.

--- test/annotation/wrapper.py
+++ test/annotation/wrapper.py
@@ -10,10 +10,18 @@ log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 # Extract parameters.
 output = snakemake.output[0]
+BASE_URL = 'https://dohlee-bioinfo.sgp1.digitaloceanspaces.com/test-data/annotation/'
+
+if 'hg38' in output:
+    url = BASE_URL + 'Homo_sapiens.hg38.annotation.gtf'
+elif 'GRCh38' in output:
+    url = BASE_URL + 'Homo_sapiens.GRCh38.annotation.gtf'
+else:
+    raise ValueError('Output annotation file name should include "hg38" or "GRCh38".')
 
 # Execute shell command.
 shell(
     "("
-    "wget https://dohlee-bioinfo.sgp1.digitaloceanspaces.com/test-data/annotation/Homo_sapiens.GRCh38.annotation.gtf -qO {output}"
+    "wget {url} -qO {output}"
     ")"
 )
