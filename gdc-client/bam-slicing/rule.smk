45ee29a3c2e06f3eb2ca435854ee5483587f794b
dohlee <dohlee.bioinfo@gmail.com>
2019-11-14 19:14:41 +0900

    Write gdc-client/bam-slicing wrapper.

---
+++ gdc-client/bam-slicing/rule.smk
@@ -0,0 +1,16 @@
+rule bam_slicing:
+    output:
+        '{sample}.bam'
+    params:
+        # Required parameter.
+        # GDC UUID for BAM file to slice.
+        uuid = '',
+        # Specify comma-separated region(s) if needed.
+        region = 'chr2:100000-200000,chr3:100000-200000',
+        # Specify comma-separated HGNC/GENCODE v22 gene name(s) if needed.
+        gencode = 'BRCA1,DNMT3A',
+    threads: 1
+    log: 'logs/bam_slicing/{sample}.log'
+    benchmark: 'benchmarks/bam_slicing/{sample}.benchmark'
+    wrapper:
+        'http://dohlee-bio.info:9193/gdc-client/bam-slicing'
---
+++ gdc-client/bam-slicing/wrapper.py
@@ -0,0 +1,39 @@
+__author__ = "Dohoon Lee"
+__copyright__ = "Copyright 2019, Dohoon Lee"
+__email__ = "dohlee.bioinfo@gmail.com"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+# Extract log.
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Extract required arguments.
+token_file = snakemake.input[0]
+with open(token_file) as inFile:
+    token = inFile.readline().strip()
+
+out = snakemake.output[0]
+
+parameters = []
+if 'region' in snakemake.params:
+    for r in snakemake.params['region'].split(','):
+        parameters.append('region=' + r)
+if 'gencode' in snakemake.params:
+    for g in snakemake.params['gencode'].split(','):
+        parameters.append('gencode=' + g)
+
+if len(parameters) != 0:
+    raise ValueError('Please specify at least one parameters for BAM slicing.')
+parameters = '&'.join(parameters)
+
+# Execute shell command.
+shell(
+    '('
+    'curl --header "X-Auth-Token: {token}" '
+    '\'https://api.gdc.cancer.gov/slicing/view/{snakemake.params.uuid}?{parameters}\' '
+    '--output {out}'
+    ') '
+    '{log}'
+)
