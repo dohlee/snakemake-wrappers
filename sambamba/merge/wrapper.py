3ca5c54d99840f4dd59ff8c969946df933866c8a
dohlee <dohlee.bioinfo@gmail.com>
2020-03-23 15:35:39 +0900

    Fix sambamba/merge wrapper to merge only if >2 BAMs are given.

--- sambamba/merge/wrapper.py
+++ sambamba/merge/wrapper.py
@@ -1,5 +1,5 @@
 __author__ = "Dohoon Lee"
-__copyright__ = "Copyright 2018, Dohoon Lee"
+__copyright__ = "Copyright 2020, Dohoon Lee"
 __email__ = "dohlee.bioinfo@gmail.com"
 __license__ = "MIT"
 
@@ -17,13 +17,20 @@ bams = snakemake.input
 merged_bam = snakemake.output
 
 # Execute shell command.
-shell(
-    "("
-    "sambamba merge "
-    "{extra} "
-    "-t {snakemake.threads} "
-    "{merged_bam} "
-    "{bams} "
-    ") "
-    "{log}"
-)
+if len(bams) > 1:
+    # If more than one BAM files are given, merge them.
+    shell(
+        "("
+        "sambamba merge "
+        "{extra} "
+        "-t {snakemake.threads} "
+        "{merged_bam} "
+        "{bams} "
+        ") "
+        "{log}"
+    )
+else:
+    # Or if only one BAM file is given, just copy it.
+    shell(
+        "cp {bams} {merged_bam}"
+    )
