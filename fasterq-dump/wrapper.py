c63604dd35123e4b30e6dc4b9a34b6623918e9c1
dohlee <dohlee.bioinfo@gmail.com>
2021-05-31 09:29:05 +0900

    Update fasterq-dump wrapper.

--- fasterq-dump/wrapper.py
+++ fasterq-dump/wrapper.py
@@ -28,13 +28,14 @@ sra_file = path.basename(str(sra))
 if len(snakemake.output) not in [1, 2]:
     raise RuleOutputException('Please specify one(single-end) or two(paired-end) fastq files to the output.\n\n\
                                Hint: *.fastq.gz for single-end, *.read1.fastq.gz and *.read2.fastq.gz for paired-end.')
-output_directory = path.dirname(snakemake.output[0]) or '.'
+out_dir = path.dirname(snakemake.output[0]) or '.'
 
+# Paired-end case.
 if len(snakemake.output) == 2:
     # Extract sample name.
     for output in snakemake.output:
         if output.endswith('.read1.fastq.gz'):
-            sample_name = output[:-15]
+            sample_name = path.basename(output)[:-15]
 
     raw_read1_file = '%s.sra_1.fastq.gz' % sample_name
     renamed_read1_file = '%s.read1.fastq.gz' % sample_name
@@ -42,25 +43,25 @@ if len(snakemake.output) == 2:
     raw_read2_file = '%s.sra_2.fastq.gz' % sample_name
     renamed_read2_file = '%s.read2.fastq.gz' % sample_name
 
-    gzip_command = '&& pigz %s.sra_1.fastq --processes %d' \
-                   '&& pigz %s.sra_2.fastq --processes %d' %\
+    gzip_command = '&& pigz %s.sra_1.fastq --processes %d ' \
+                   '&& pigz %s.sra_2.fastq --processes %d ' %\
                    (sample_name, snakemake.threads,
                     sample_name, snakemake.threads)
 
-    rename_command = '&& mv %s %s ' \
-                     '&& mv %s %s ' %\
-                     (raw_read1_file, renamed_read1_file,
-                      raw_read2_file, renamed_read2_file)
+    rename_command = '&& mv %s/%s %s/%s ' \
+                     '&& mv %s/%s %s/%s ' %\
+                     (sra_dir, raw_read1_file, out_dir, renamed_read1_file,
+                      sra_dir, raw_read2_file, out_dir, renamed_read2_file)
 
 # NOTE: For single-end case, we rename *_pass.fastq.gz into *.fastq.gz.
 else:
     # Extract sample name from *.fastq.gz
-    sample_name = snakemake.output[0][:-9]
+    sample_name = path.basename(snakemake.output[0])[:-9]
     raw_read_file = '%s.sra.fastq.gz' % sample_name
     renamed_read_file = '%s.fastq.gz' % sample_name
 
-    gzip_command = '&& pigz %s.sra.fastq --processes %d' % (sample_name, snakemake.threads)
-    rename_command = '&& mv %s %s' % (raw_read_file, renamed_read_file)
+    gzip_command = '&& pigz %s.sra.fastq --processes %d ' % (sample_name, snakemake.threads)
+    rename_command = '&& mv %s/%s %s/%s' % (sra_dir, raw_read_file, out_dir, renamed_read_file)
 
 # NOTE: I fixed some recommended options for fastq-dump.
 # Refer to: `fastq-dump-best-practice` in https://github.com/dohlee/bioinformatics-one-liners.
@@ -71,10 +72,11 @@ shell(
     "fasterq-dump "
     "{sra_file} "
     "--threads {snakemake.threads} "
-    "--outdir {output_directory} "
+    "--outdir . "
     "--split-3 --skip-technical "
     "{extra} "
     "{gzip_command} "
+    "&& cd - "
     "{rename_command} "
     ") "
     "{log} "
